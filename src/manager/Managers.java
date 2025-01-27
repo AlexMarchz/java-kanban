5415f769793e8dde14d6f63d8f0753460a255f9b
Krakafima <san.sliadnev@yandex.ru>
2025-01-28 02:06:27 +0300

    CODESTYLE

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -8,11 +8,11 @@ public class Managers {
         return FileBackedTaskManager.loadFromFile(file);
     }
 
-    public static TaskManager getDefault(File file) {
-        return new FileBackedTaskManager(file);
-    }
-
     public static InMemoryHistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
+    }
+
+    public static TaskManager getDefault(File file) {
+        return new FileBackedTaskManager(file);
     }
 }
